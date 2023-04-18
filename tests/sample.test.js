943b981c1ae5998a072b850e51a4f60f38f02e5c
raed rdhaounia <raedrdhaounia@gmail.com>
2023-04-19 00:22:49 +0100

    fix test

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,5 +1,5 @@
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
-    })
+describe('first test for jest', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
   })
+})
