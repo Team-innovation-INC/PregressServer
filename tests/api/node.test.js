3779bb09f1cea6a1135ec20e91b90a3bc1f38a8d
raed rdhaounia <raedrdhaounia@gmail.com>
2023-04-19 02:04:23 +0100

    Update node.test.js

--- tests/api/node.test.js
+++ tests/api/node.test.js
@@ -1,6 +1,20 @@
 const request = require("supertest");
-const app = require("../..");
+const app = require("../../index")
+const {connect, disconnect} = require("../config/connectDB")
 
+let server; // Define a variable to hold the server instance
+
+beforeAll(async () => {
+    await connect();
+    server = app.listen(5000); // Start the server and store the server instance
+
+  });
+
+  afterAll(async () => {
+    await disconnect();
+    server.close(); // Stop the server after all tests have run
+  });
+  
   describe("Test the root path", () => {
     test("It should response the GET method", done => {
       request(app)
